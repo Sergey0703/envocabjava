8f74645bdf63304bfe5e55932aa66f8ddc9c513c
Baliasnyi Serghii <sergey070373@gmail.com>
2023-05-27 21:14:18 +0100

    Recycler View Ok222

--- app/src/main/java/com/example/envocab/SoundActivity.java
+++ app/src/main/java/com/example/envocab/SoundActivity.java
@@ -5,10 +5,15 @@ import static android.app.PendingIntent.getActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -19,14 +24,20 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SoundActivity extends BaseActivity {
     //    private static final int MENU3 = 1;
     private static final String TAG = "SoundActivity";
+    boolean isLoading = false;
     private RecyclerView wordsList;
     private WordsAdapter wordsAdapter;
     private List<Word> listWords;
-
+    LinearLayoutManager layoutManager;
+    ImageButton btnPlaySound;
+    TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,43 +45,204 @@ public class SoundActivity extends BaseActivity {
         setContentView(R.layout.activity_sound);
 
         wordsList = findViewById(R.id.rv_words);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager = new LinearLayoutManager(this);
         wordsList.setLayoutManager(layoutManager);
+        btnPlaySound = findViewById(R.id.btnPlaySound);
 
+        btnPlaySound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("testLogs", "Speech");
+                playAutoSound3();
+
+            }
+        });
+
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+
+                // if No error is found then only it will run
+                if(i!=TextToSpeech.ERROR){
+                    // To Choose language of speech
+                    textToSpeech.setLanguage(Locale.UK);
+                }
+            }
+        });
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                LocalDate today = LocalDate.now() ;
+                LocalDate today = LocalDate.now();
                 LocalDateTime startOfDate = today.atStartOfDay();
                 LocalDateTime endOfDate = LocalTime.MAX.atDate(today);
 
                 ZonedDateTime zdtStart = ZonedDateTime.of(startOfDate, ZoneId.systemDefault());
                 ZonedDateTime zdtEnd = ZonedDateTime.of(endOfDate, ZoneId.systemDefault());
-                Long startOfDay=zdtStart.toInstant().toEpochMilli();
-                Long endOfDay=zdtEnd.toInstant().toEpochMilli();
+                Long startOfDay = zdtStart.toInstant().toEpochMilli();
+                Long endOfDay = zdtEnd.toInstant().toEpochMilli();
 
-                 listWords = AppDatabase.getInstance(getApplicationContext())
-                         .wordDao()
-                         .wordsForList(startOfDay,endOfDay, 1);
-                 //for(Word w: listWords){
-                    // Log.d(TAG,w.toString());
+                listWords = AppDatabase.getInstance(getApplicationContext())
+                        .wordDao()
+                        .wordsForList(startOfDay, endOfDay, 1);
+                //for(Word w: listWords){
+                // Log.d(TAG,w.toString());
                 //}
 
             }
         });
         thread.start();
 
-            new Handler(Looper.getMainLooper()).post(new Runnable() {
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
 
-                @Override
-                public void run() {
-                    if (listWords.size() != 0) {
+            @Override
+            public void run() {
+                if (listWords.size() != 0) {
                     wordsList.setHasFixedSize(true);
                     wordsAdapter = new WordsAdapter(listWords);
                     wordsList.setAdapter(wordsAdapter);
+                    initScrollListener();
                 }
             }
-            });
+        });
+    }
+    private void initScrollListener() {
+        wordsList.addOnScrollListener(new RecyclerView.OnScrollListener() {
+                                          @Override
+                                          public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                                              super.onScrollStateChanged(recyclerView, newState);
+                                             // System.out.println("onScrollStateChanged");
+                                              LinearLayoutManager linearLayoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+
+                                              View v = layoutManager.findViewByPosition(linearLayoutManager.findFirstCompletelyVisibleItemPosition());
+                                              TextView textViewName
+                                                      = (TextView) v.findViewById(R.id.tv_number_item);
+                                              String selectedName = (String) textViewName.getText();
+                                              System.out.println("onScrollStateChanged="+selectedName);
+                                              //R.id.tv_number_item).toString());
+                                          }
+                                          @Override
+                                          public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                                              super.onScrolled(recyclerView, dx, dy);
+                                              //System.out.println("onScrolled1");
+                                              LinearLayoutManager linearLayoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+
+                                              if (!isLoading) {
+                                                  System.out.println("onScrolled---222 "+linearLayoutManager.findFirstCompletelyVisibleItemPosition()+" "+linearLayoutManager.findLastCompletelyVisibleItemPosition());
+                                                  if (linearLayoutManager != null && linearLayoutManager.findLastCompletelyVisibleItemPosition() == listWords.size() - 1) {
+                                                      // bottom of list!
+                                                      System.out.println("onScrolled222");
+                                                       loadMore();
+                                                      isLoading = true;
+                                                  }
+                                              }
+                                          }
+                                      }
+        );
+    }
+    private void loadMore() {
+      //  listWords.add(null);
+      //  wordsAdapter.notifyItemInserted(listWords.size() - 1);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+              //  listWords.remove(listWords.size() - 1);
+                int scrollPosition = listWords.size();
+                System.out.println("loadMore");
+              // wordsAdapter.notifyItemRemoved(scrollPosition);
+                int currentSize = scrollPosition;
+                System.out.println("loadMore1111");
+                // Next load more option is to be shown after every 10 items.
+                int nextLimit = currentSize + 10;
+
+                while (currentSize - 1 < nextLimit) {
+                    //listWords.add("Item " + currentSize);
+                    currentSize++;
+                }
+
+                wordsAdapter.notifyDataSetChanged();
+                isLoading = false;
+                System.out.println("Load more!");
+            }
+
+        }, 2000);
+    }
+
+    public void playAutoSound3(){
+
+    }
+
+    public void playAutoSound(){
+        final Handler handler = new Handler();
+        Timer timer=new Timer();
+        timer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+            if(layoutManager.findLastVisibleItemPosition()<(wordsAdapter.getItemCount()-1)){
+                layoutManager.smoothScrollToPosition(wordsList,new RecyclerView.State(),layoutManager.findLastCompletelyVisibleItemPosition()+1);
+            //}else if(layoutManager.findLastVisibleItemPosition()==(wordsAdapter.getItemCount()-1)){
+            }else {
+                layoutManager.smoothScrollToPosition(wordsList,new RecyclerView.State(),0);
+            }
+            //layoutManager.getFocusedChild().
+            //Log.d("Tag","Pos="+layoutManager.findLastVisibleItemPosition());
+                System.out.println("TagLogsOne");
+            }
+            //int ind=getAdapterPosition();
+        },0,3000);
+    }
+    public void playAutoSound2(){
+
+        final int speedScroll = 1200;
+        final Handler handler = new Handler();
+        System.out.println("Before00000");
+        View  loc=wordsList.getChildAt(8);
+        System.out.println("Before1111"+loc);
+       // RecyclerView.ViewHolder holder = wordsList.getChildViewHolder(loc);
+        System.out.println("Before");
+       // TextView txtName = holder.itemView.findViewById(R.id.tv_number_item);
+        System.out.println("After");
+      //  System.out.println("TTT="+txtName.getText().toString());
+        final Runnable runnable = new Runnable() {
+            int count = 0;
+            boolean flag = true;
+
+
+            @Override
+            public void run() {
+                if(layoutManager.findLastVisibleItemPosition()<(wordsAdapter.getItemCount()-1)){
+                    layoutManager.smoothScrollToPosition(wordsList,new RecyclerView.State(),layoutManager.findLastCompletelyVisibleItemPosition()+1);
+                    //}else if(layoutManager.findLastVisibleItemPosition()==(wordsAdapter.getItemCount()-1)){
+                }else {
+                    layoutManager.smoothScrollToPosition(wordsList,new RecyclerView.State(),0);
+                }
+                //Log.d("testLogs",String.valueOf(wordsAdapter.));
+                //String title = ((TextView) wordsList.findViewHolderForAdapterPosition(0).itemView.findViewById(R.id.tv_number_item)).getText().toString();
+                //wordsList.findViewHolderForAdapterPosition(0).itemView.findViewById()
+                //Log.d("testLogs",title);
+                handler.postDelayed(this,speedScroll);
+//                if(count < wordsAdapter.getItemCount()){
+//                    if(count==wordsAdapter.getItemCount()-1){
+//                        flag = false;
+//                    }else if(count == 0){
+//                        flag = true;
+//                    }
+//                    if(flag) count++;
+//                    else count--;
+//
+//                    wordsList.smoothScrollToPosition(count);
+//                    Log.d("testLogs", String.valueOf(count));
+//                    handler.postDelayed(this,speedScroll);
+//                }
+            }
+        };
+
+        handler.postDelayed(runnable,speedScroll);
+    }
+
+
 
         //    @Override
 //    public boolean onPrepareOptionsMenu(Menu menu) {
@@ -95,5 +267,5 @@ public class SoundActivity extends BaseActivity {
 //    }
 //
 //}
-    }
+
 }
--- app/src/main/java/com/example/envocab/WordsAdapter.java
+++ app/src/main/java/com/example/envocab/WordsAdapter.java
@@ -1,9 +1,11 @@
 package com.example.envocab;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,6 +14,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
 public class WordsAdapter extends RecyclerView.Adapter<WordsAdapter.WordViewHolder>{
+    private final int VIEW_TYPE_ITEM = 0;
+    private final int VIEW_TYPE_LOADING = 1;
     private static int viewHolderCount;
     private int numberItems;
     private List<Word> wordsList;
@@ -23,21 +27,47 @@ public class WordsAdapter extends RecyclerView.Adapter<WordsAdapter.WordViewHold
         this.wordsList=wordsList;
     }
 
+
+
     @NonNull
     @Override
     public WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         Context context= parent.getContext();
         int layoutIdForListItem=R.layout.word_list_item;
 
-        LayoutInflater inflater=LayoutInflater.from(context);
-        View view=inflater.inflate(layoutIdForListItem, parent, false);
-        WordViewHolder viewHolder=new WordViewHolder(view);
-        //viewHolder.viewHolderIndex.setText("ViewHolder index="+viewHolderCount);
-        //viewHolderCount++;
-        return viewHolder;
+        if (viewType == VIEW_TYPE_ITEM) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.word_list_item, parent, false);
+            return new WordViewHolder(view);
+        } else {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_loading, parent, false);
+            return new WordViewHolder(view);
+
+        }
 
     }
+    public class ItemViewHolder extends RecyclerView.ViewHolder {
+        TextView tvItem;
+        public ItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvItem = itemView.findViewById(R.id.tv_number_item);
+        }
+    }
 
+    private class LoadingviewHolder extends RecyclerView.ViewHolder {
+        ProgressBar progressBar;
+        public LoadingviewHolder(@NonNull View itemView) {
+            super(itemView);
+            //progressBar = itemView.findViewById(R.id.progressbar);
+        }
+    }
+
+    public int getItemViewType(int position) {
+        return wordsList.get(position) == null ? VIEW_TYPE_LOADING : VIEW_TYPE_ITEM;
+    }
+//    private void populateItemRows(ItemViewHolder viewHolder, int position) {
+//        String item = wordsList.get(position);
+//        viewHolder.tvItem.setText(item);
+//    }
     @Override
     public void onBindViewHolder(@NonNull WordViewHolder holder, int position) {
         //holder.bind(position);
@@ -62,6 +92,8 @@ public class WordsAdapter extends RecyclerView.Adapter<WordsAdapter.WordViewHold
 
             listItemNumberView=itemView.findViewById(R.id.tv_number_item);
             viewHolderIndex=itemView.findViewById(R.id.tv_holder_number);
+            //Log.d("Tag",String.valueOf(listItemNumberView.getText() ));
+            //int ind=getAdapterPosition();
 
         }
 
